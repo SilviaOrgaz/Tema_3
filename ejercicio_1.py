a1b62075f6c0bb53d6ff5309e9208234e5a5acba
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-26 22:59:37 +0200

    estoy modificando el codigo

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,4 +1,4 @@
-#Para realizar de forma recursiva este ejercicio hay que hacer dos por el anterior más uno
+#Para realizar de forma recursiva este ejercicio hay que hacer dos por el anterior más uno.
 def hanoi(n):
     if n == 1:
         contador = 1
@@ -6,7 +6,6 @@ def hanoi(n):
         contador = (2* hanoi(n-1)+1)
     return contador
 
-def imprimir():
-    n = input("Introduce el número de discos de la Torre Hanói: ")
-    print("{} discos se tienen que mover {} veces.".format(n, hanoi(n)))
+n = int(input("Introduce el número de discos de la Torre Hanói: "))
+print("{} discos se tienen que mover {} veces.".format(n, hanoi(n)))
 
