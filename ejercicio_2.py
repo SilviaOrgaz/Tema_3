2ef7334eddd992fd3a521fd288b097b327be7d5d
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-26 20:04:57 +0200

    he hecho cambios

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -6,11 +6,6 @@ def crear_matriz(numero_filas, numero_columnas):
         matriz[i] = [None] * numero_columnas
     return matriz
 
-
-#Calculo de un determinante de una matriz de grado 2
-def determinante_grado_2(m2):
-    return m2[0][0] * m2[1][1] - m2[0][1] * m2[1][0]
-
 #Genero una matriz de grado dos de cuatro elementos
 def insertar_elementos_grado_2(a, b, c , d):
     matriz = crear_matriz(2, 2)
@@ -20,12 +15,20 @@ def insertar_elementos_grado_2(a, b, c , d):
     matriz[1][1] = d
     return matriz
 
+#Calculo de un determinante de una matriz de grado 2
+def determinante_grado_2(m2):
+    return m2[0][0] * m2[1][1] - m2[0][1] * m2[1][0]
+
+
 #Función iterativa
 def determinante_grado_3(m3):
     A1 = m3[0][0] * determinante_grado_2(insertar_elementos_grado_2(m3[1][1], m3[1][2], m3[2][1], m3[2][2]))
     A2 = m3[0][1] * determinante_grado_2(insertar_elementos_grado_2(m3[1][0], m3[1][2], m3[2][0], m3[2][2]))
     A3 = m3[0][2] * determinante_grado_2(insertar_elementos_grado_2(m3[1][0], m3[1][1], m3[2][0], m3[2][1]))
     return A1 - A2 + A3
+
+def determinante_grado_3_sarrus(m3):
+
 
 matriz = [[1, 2, 4],[2, 3, 5],[8, 5, 9]]
 print("El determinante es:",determinante_grado_3(matriz))
