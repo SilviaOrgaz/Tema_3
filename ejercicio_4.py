659bb25d4603d46cc1074501e339c69c7e115928
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-11-06 21:28:40 +0100

    he creado la clase polinomio

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -7,3 +7,11 @@ class datoPolinomio(object):
     def __init__(self, valor, termino):
         self.valor = valor
         self.termino = termino
+
+class Polinomio(object):
+
+    def __init__(self):
+        self.termino_mayor = None
+        self.ggrado = -1
+
+    def agregar_termino(self, termino, valor):
