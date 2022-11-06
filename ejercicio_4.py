83a2408d674029cee4eca73e5dcbacff5462ef29
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-11-06 21:26:55 +0100

    he creado la clase polinomio

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -1,3 +1,9 @@
 #He creado este ejercicio en el repositrio de ejercicio_coches
 class Nodo(object):
     info, sig = None, None
+
+class datoPolinomio(object):
+
+    def __init__(self, valor, termino):
+        self.valor = valor
+        self.termino = termino
