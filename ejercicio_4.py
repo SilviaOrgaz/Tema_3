0c61fcaec78a2400b049e54a07ce3d7cc97d9866
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-11-06 21:32:03 +0100

    he creado la funcion agregar termino

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -12,6 +12,19 @@ class Polinomio(object):
 
     def __init__(self):
         self.termino_mayor = None
-        self.ggrado = -1
+        self.grado = -1
 
     def agregar_termino(self, termino, valor):
+        aux = Nodo()
+        dato= datoPolinomio(valor, termino)
+        aux.info=dato
+        if(termino > self.grado):
+            aux.sig = self.termino_mayor
+            self.termino_mayor = aux
+            self.grado = termino
+        else:
+            actual = self.termino_mayor
+            while(actual.sig is not None and termino < actual.sig.info.termino):
+                actual = actual.sig
+            aux.sig = actual.sig
+            actual.sig = aux
